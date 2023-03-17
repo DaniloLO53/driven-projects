1f8c261d006bfebc738a774b9aa99722bf0b0dea
Danilo Leão <olin_ad@hotmail.com>
2023-03-17 17:53:50 -0300

    tweteroo

---
+++ src/app.js
@@ -0,0 +1,68 @@
+import express from 'express';
+import cors from 'cors';
+import tweets from './model/tweet.model.js';
+import users from './model/user.model.js';
+
+const PORT = 5000;
+
+const CODE_BAD_REQUEST = 400;
+const CODE_OK = 200;
+const CODE_CREATED = 201;
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+server.post('/sign-up', (request, response) => {
+  if (
+    !request.body.username
+    || !request.body.avatar
+    || typeof request.body.username !== 'string'
+    || typeof request.body.avatar !== 'string'
+  ) {
+    return response.status(CODE_BAD_REQUEST).send('Todos os campos são obrigatórios!');
+  }
+
+  users.push(request.body);
+
+  response.status(CODE_CREATED).send(request.body);
+  response.send('OK');
+});
+
+server.post('/tweets', (request, response) => {
+  if (!request.body.username || !request.body.tweet || typeof request.body.tweet !== 'string') {
+    return response.status(CODE_BAD_REQUEST).send('Todos os campos são obrigatórios!');
+  }
+
+  const user = request.body.username;
+  const isSignedUp = users.some(({ username }) => username === user);
+
+  if (!isSignedUp) {
+    return response.status(401).send('UNAUTHORIZED');
+  }
+
+  tweets.push(request.body);
+
+  response.status(CODE_CREATED).send(request.body);
+  response.send('OK');
+});
+
+server.get('/tweets', (request, response) => {
+  const last10Tweets = tweets.slice(tweets.length - 10);
+  const avatars = last10Tweets.map((tweet) => {
+    return users.find(({ username }) => tweet.username === username).avatar
+  });
+
+  last10Tweets.forEach((tweet, index) => tweet.avatar = avatars[index]);
+
+  response.send(last10Tweets);
+});
+
+server.get('/tweets/:USERNAME', (request, response) => {
+  const userName = request.params.USERNAME;
+  const tweetsFromUser = tweets.filter(({ username }) => username === userName);
+
+  response.status(CODE_OK).send(tweetsFromUser);
+});
+
+server.listen(PORT, () => console.log('Listening to port ', PORT));
---
+++ src/model/tweet.model.js
@@ -0,0 +1,5 @@
+const tweets = [
+
+];
+
+export default tweets;
